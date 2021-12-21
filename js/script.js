8a2401316cf428fb74ac82d0757716b7ac2c0c9b
Frampolz <francesco.colloca@hotmail.it>
2021-12-21 15:10:41 +0100

    print message via js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 /*Milestone 1
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto */
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+
+*/
 const app = new Vue({
   el: '#app',
   data: {
+    counter: 0,
     contacts: [
      {
        name: "Michele",
@@ -91,5 +98,11 @@ const app = new Vue({
      },
    ],
     
-  }
+  },
+  methods: {
+    
+  },
+   created() {
+    console.log(this.contacts[0].messages)
+  }, 
 });
